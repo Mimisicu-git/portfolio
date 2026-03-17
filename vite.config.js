4a95c528eeaee56aa86862c6dedabb2208c512f8
sopenar <remy.sopena@etu.iut-grenoble-alpes.fr>
2026-03-17 12:24:35 +0100

    :spakles: premiere route homepage helloworld

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { serve } from 'pm2'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+  }
 })
