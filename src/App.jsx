014087b8ddae964fbe486d0791b49d0277ef060c
sopenar <remy.sopena@gmail.com>
2026-06-12 11:58:55 +0200

    :beers: push finale en principe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProjectPage from './pages/ProjectPage'
 import ContactPage from './pages/ContactPage'
@@ -10,35 +10,49 @@ import DetailsProject from './pages/DetailsProjectPage'
 import { MoodvieRedirect } from './utils/MoodvieRedirect'
 import './App.css'
 
-function App() {
+function AppContent() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectPath = sessionStorage.getItem('redirect_path');
+    if (redirectPath) {
+      sessionStorage.removeItem('redirect_path');
+      navigate(redirectPath);
+    }
+  }, [navigate]);
 
   return (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomePage />}
+      />
+      <Route
+        path="/projects"
+        element={<ProjectPage />}
+      />
+      <Route
+        path="/contacts"
+        element={<ContactPage />}
+      />
+      <Route
+        path="/moodvie"
+        element={<MoodvieRedirect />}
+      />
+      <Route
+        path="/projects/:id"
+        element={<DetailsProject />}
+      />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
     <BrowserRouter basename="/portfolio/">
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage />}
-        />
-        <Route
-          path="/projects"
-          element={<ProjectPage />}
-        />
-        <Route
-          path="/contacts"
-          element={<ContactPage />}
-        />
-        <Route
-          path="/moodvie"
-          element={<MoodvieRedirect />}
-        />
-        <Route
-          path="/projects/:id"
-          element={<DetailsProject />}
-        />
-        
-      </Routes>
+      <AppContent />
     </BrowserRouter>
-      )
+  );
 }
 
 export default App
