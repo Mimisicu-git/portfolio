435e1631ed5b11174fe1a5f95a56780bb317150d
sopenar <remy.sopena@gmail.com>
2026-07-16 09:58:44 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
-  server: {
+  base: '/',
+  server: { 
     port: 5174,
   },
   build: {
